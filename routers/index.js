7e80552d00690f1debb63ee2bd1d81d1c7b43d84
aakash018 <aakashkhanal015@gmail.com>
2020-05-28 00:03:20 +0545

    Calc First Phase

--- routers/index.js
+++ routers/index.js
@@ -18,7 +18,6 @@ router.post("/", async (req, res) => {
   })
   try {
     const newEntry = await entry.save();
-    const newAudit = await audits.save()
     res.redirect("/infoTable");
   } catch {
     res.render("index", {
@@ -29,9 +28,31 @@ router.post("/", async (req, res) => {
 });
 
 const calculation = async (amount, person) => {
-  const audits = await Audits.find({})
+  const amount_divides = amount / 3;
   if (person === "Aakash") {
-    console.log(audits[audits.length - 1].Subash_Aakash)
+    const audits_calc = await Audits.find({})
+    const diff_subash = await audits_calc[audits_calc.length - 1].Aakash_Subash - amount_divides
+    if (diff_subash < 0) {
+      var add_subash = audits_calc[audits_calc.length - 1].Subash_Aakash + Math.abs(diff_subash)
+    } else {
+      var add_subash = 25;
+    }
+    const diff_yaman = await audits_calc[audits_calc.length - 1].Aakash_Yaman - amount_divides
+    if (diff_yaman < 0) {
+      var add_yaman = audits_calc[audits_calc.length - 1].Yaman_Aakash + Math.abs(diff_yaman)
+    } else {
+      var add_yaman = 25;
+    }
+    const audit = await new Audits({
+      Subash_Aakash: add_subash,
+      Yaman_Aakash: add_yaman,
+    })
+    try {
+      await audit.save()
+    } catch (err) {
+      res.redirect("/");
+      console.log(err)
+    }
   } else if (person === "Subash") {
     console.log("hola")
   } else {
